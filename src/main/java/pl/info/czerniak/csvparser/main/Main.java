02893f1f2976ce4e6180d753cd0a5593a7245068
Andrzej Czerniak <andrzej.czerniak@gmail.com>
2020-04-20 22:50:41 +0200

    Removed code for writing records to output csv file and add code to write it to database.

--- src/main/java/pl/info/czerniak/csvparser/main/Main.java
+++ src/main/java/pl/info/czerniak/csvparser/main/Main.java
@@ -2,6 +2,7 @@ package pl.info.czerniak.csvparser.main;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import pl.info.czerniak.csvparser.dao.PostgresTestDAOImpl;
 import pl.info.czerniak.csvparser.exception.IncorrectColNumberFileException;
 import pl.info.czerniak.csvparser.parser.CSVFileParserFactory;
 import pl.info.czerniak.csvparser.parser.CSVValidator;
@@ -14,10 +15,11 @@ import pl.info.czerniak.csvparser.printer.PrinterFactory;
 import javax.naming.directory.NoSuchAttributeException;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) throws NoSuchAttributeException, IncorrectColNumberFileException, IOException {
+    public static void main(String[] args) throws NoSuchAttributeException, IncorrectColNumberFileException, IOException, SQLException {
 
         String inputFile = System.getProperty("user.dir")+"/doc/sample/input.csv";
         String outputFile = System.getProperty("user.dir")+"/doc/sample/output.csv";
@@ -25,8 +27,12 @@ public class Main {
         ParserFactory parserFactory = ParserFactory.getInstance(FactoryType.FILE_FACTORY);
         CSVFileParserFactory fileParserFactory = parserFactory.getCSVFileParserFactory();
         CSVParser csvParser = fileParserFactory.getParserForFileName(inputFile, new CSVValidator(Charset.defaultCharset(), 10, false, CSVFormat.DEFAULT.withHeader()));
-        FilePrinterFactory filePrinterFactory = PrinterFactory.getInstance(FactoryType.FILE_FACTORY);
-        CSVFilePrinterFactory csvFilePrinterFactory = filePrinterFactory.getCSVFilePrinterFactory();
-        csvFilePrinterFactory.printToFile(csvParser,CSVFormat.POSTGRESQL_CSV, outputFile);
+        PostgresTestDAOImpl dao = new PostgresTestDAOImpl();
+        dao.create(csvParser);
+        /*
+            FilePrinterFactory filePrinterFactory = PrinterFactory.getInstance(FactoryType.FILE_FACTORY);
+            CSVFilePrinterFactory csvFilePrinterFactory = filePrinterFactory.getCSVFilePrinterFactory();
+            csvFilePrinterFactory.printToFile(csvParser,CSVFormat.POSTGRESQL_CSV, outputFile);
+         */
     }
 }
