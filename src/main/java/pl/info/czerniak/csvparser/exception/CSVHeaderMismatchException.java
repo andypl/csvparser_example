9806cc1958e054e717603deb11ba9173a2157a50
Andrzej Czerniak <andrzej.czerniak@gmail.com>
2020-04-21 22:32:09 +0200

    Add CSVHeaderMismatchException class.

---
+++ src/main/java/pl/info/czerniak/csvparser/exception/CSVHeaderMismatchException.java
@@ -0,0 +1,9 @@
+package pl.info.czerniak.csvparser.exception;
+
+public class CSVHeaderMismatchException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public CSVHeaderMismatchException(String message) {
+        super(message);
+    }
+}
