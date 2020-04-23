2d83a53b968712e56ddf194dea01442467f2b041
Andrzej Czerniak <andrzej.czerniak@gmail.com>
2020-04-23 23:28:23 +0200

    Removed validation method. It will be added in future.

--- src/main/java/pl/info/czerniak/csvparser/parser3/CSVFileParser.java
+++ src/main/java/pl/info/czerniak/csvparser/parser3/CSVFileParser.java
@@ -2,10 +2,11 @@ package pl.info.czerniak.csvparser.parser3;
 
 import org.apache.commons.csv.CSVParser;
 
-import java.io.File;
+import java.io.IOException;
 
+/**
+ * Interface for Parser classes.
+ */
 public interface CSVFileParser {
-    File readFileForParse(String filName);
-    boolean validateFile(File file, CSVFileSpec csvFileSpec);
-    CSVParser parse(File file, CSVFileSpec csvFileSpec);
+     void parse() throws IOException;
 }
