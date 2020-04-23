9d1d6a92b0b9101c83276a7fcfe1629d7b794eee
Andrzej Czerniak <andrzej.czerniak@gmail.com>
2020-04-23 23:33:34 +0200

    Changed main class for use new classes.

--- src/main/java/pl/info/czerniak/csvparser/main/Main.java
+++ src/main/java/pl/info/czerniak/csvparser/main/Main.java
@@ -1,38 +1,34 @@
 package pl.info.czerniak.csvparser.main;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import pl.info.czerniak.csvparser.dao.PostgresTestDAOImpl;
+import pl.info.czerniak.csvparser.converter.ConverterImpl;
+import pl.info.czerniak.csvparser.dao.DatabaseManager;
+import pl.info.czerniak.csvparser.database.ConnectionManager;
 import pl.info.czerniak.csvparser.exception.IncorrectColNumberFileException;
-import pl.info.czerniak.csvparser.parser.CSVFileParserFactory;
-import pl.info.czerniak.csvparser.parser.CSVValidator;
-import pl.info.czerniak.csvparser.parser.FactoryType;
-import pl.info.czerniak.csvparser.parser.ParserFactory;
-import pl.info.czerniak.csvparser.printer.CSVFilePrinterFactory;
-import pl.info.czerniak.csvparser.printer.FilePrinterFactory;
-import pl.info.czerniak.csvparser.printer.PrinterFactory;
+import pl.info.czerniak.csvparser.parser3.CSVFileParserImpl;
+import pl.info.czerniak.csvparser.service.ParserService;
 
+import javax.management.AttributeNotFoundException;
 import javax.naming.directory.NoSuchAttributeException;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
-    public static void main(String[] args) throws NoSuchAttributeException, IncorrectColNumberFileException, IOException, SQLException {
+    public static void main(String[] args) throws NoSuchAttributeException, IncorrectColNumberFileException, IOException, SQLException, AttributeNotFoundException {
 
         String inputFile = System.getProperty("user.dir")+"/doc/sample/input.csv";
-        String outputFile = System.getProperty("user.dir")+"/doc/sample/output.csv";
 
-        ParserFactory parserFactory = ParserFactory.getInstance(FactoryType.FILE_FACTORY);
-        CSVFileParserFactory fileParserFactory = parserFactory.getCSVFileParserFactory();
-        CSVParser csvParser = fileParserFactory.getParserForFileName(inputFile, new CSVValidator(Charset.defaultCharset(), 10, false, CSVFormat.DEFAULT.withHeader()));
-        PostgresTestDAOImpl dao = new PostgresTestDAOImpl();
-        dao.create(csvParser);
-        /*
-            FilePrinterFactory filePrinterFactory = PrinterFactory.getInstance(FactoryType.FILE_FACTORY);
-            CSVFilePrinterFactory csvFilePrinterFactory = filePrinterFactory.getCSVFilePrinterFactory();
-            csvFilePrinterFactory.printToFile(csvParser,CSVFormat.POSTGRESQL_CSV, outputFile);
-         */
+        List<String> headerList = Stream.of("Header1", "Header2", "Header3", "Header4", "Header5","Header6", "Header7", "Header8", "Header9", "Header10")
+                .collect(Collectors.toList());
+
+        ParserService parserService = new ParserService(inputFile, headerList);
+        DatabaseManager databaseManager = new DatabaseManager(ConnectionManager.getInstance().getConnection());
+        ConverterImpl converter = new ConverterImpl(parserService.getCSVParser(), databaseManager.getCopyIn());
+        converter.convert();
+        System.out.println("Total rows in file: " + converter.getRows());
+        System.out.println("Rows converted: " + converter.getConvertedRows());
     }
 }
