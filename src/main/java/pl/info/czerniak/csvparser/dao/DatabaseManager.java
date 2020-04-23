70a53087e56b7e68ecb70751a07bdef09eb1ae5f
Andrzej Czerniak <andrzej.czerniak@gmail.com>
2020-04-23 23:32:00 +0200

    Implment class for obtaining COPY API.

---
+++ src/main/java/pl/info/czerniak/csvparser/dao/DatabaseManager.java
@@ -0,0 +1,33 @@
+package pl.info.czerniak.csvparser.dao;
+
+import org.postgresql.copy.CopyIn;
+import org.postgresql.copy.CopyManager;
+import org.postgresql.jdbc.PgConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * This class will act like a DAO layer.
+ * It return an copyIn object for writing to postgres using COPY API.
+ */
+public class DatabaseManager {
+
+    private static final String POSTGRES_COPY_COMMAND = "COPY test FROM STDIN WITH DELIMITER ','";
+
+    PgConnection pgConnection;
+    Connection connection;
+    CopyManager manager;
+    CopyIn copyIn;
+
+    public DatabaseManager(Connection connection) throws SQLException {
+        this.connection = connection;
+        this.pgConnection = this.connection.unwrap(PgConnection.class);
+        this.manager = new CopyManager(pgConnection);
+        this.copyIn = manager.copyIn(POSTGRES_COPY_COMMAND);
+    }
+
+    public CopyIn getCopyIn() {
+        return this.copyIn;
+    }
+}
