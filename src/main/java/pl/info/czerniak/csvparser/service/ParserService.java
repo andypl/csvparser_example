e4555f70b40681a734b83cda8733bc5a0d5258a2
Andrzej Czerniak <andrzej.czerniak@gmail.com>
2020-04-23 23:34:25 +0200

    Implement service class for creating parser for csv file using CSVFileParserImpl.

---
+++ src/main/java/pl/info/czerniak/csvparser/service/ParserService.java
@@ -0,0 +1,39 @@
+package pl.info.czerniak.csvparser.service;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import pl.info.czerniak.csvparser.parser3.CSVFileParserImpl;
+import pl.info.czerniak.csvparser.parser3.CSVFileSpec;
+
+import java.nio.charset.Charset;
+import java.util.List;
+
+/**
+ * This class is something like a wrapper to a CSVFileParserImpl.
+ * It hide details about it and simply return an CSVParser object for future use.
+ */
+public class ParserService {
+
+    private String fileName;
+    private List<String> headerList;
+    private CSVFileSpec csvFileSpec;
+
+
+    public ParserService(String fileName, List<String> headerList){
+        this.fileName = fileName;
+        this.headerList = headerList;
+
+        csvFileSpec = CSVFileSpec.Builder.builder()
+                .columnCount(10)
+                .charset(Charset.defaultCharset())
+                .csvFormat(CSVFormat.DEFAULT)
+                .headerNames(headerList)
+                .build();
+    }
+
+    public CSVParser getCSVParser(){
+        CSVFileParserImpl csvFileParser = new CSVFileParserImpl(fileName, csvFileSpec);
+        return csvFileParser.getCsvParser();
+    }
+
+}
