660f76a80bfea6f2fe901962946eb3c04ab886dd
Andrzej Czerniak <andrzej.czerniak@gmail.com>
2020-04-23 23:30:36 +0200

    Removed old code.

--- src/main/java/pl/info/czerniak/csvparser/dao/DAOFactory.java
+++
@@ -1,7 +0,0 @@
-package pl.info.czerniak.csvparser.dao;
-
-import java.sql.SQLException;
-
-public abstract class DAOFactory {
-    public abstract TestDAO getTestDAO() throws SQLException;
-}
--- src/main/java/pl/info/czerniak/csvparser/dao/TestDAO.java
+++
@@ -1,9 +0,0 @@
-package pl.info.czerniak.csvparser.dao;
-
-import org.apache.commons.csv.CSVParser;
-
-import java.sql.SQLException;
-
-public interface TestDAO {
-    public void create(CSVParser csvParser) throws SQLException;
-}
