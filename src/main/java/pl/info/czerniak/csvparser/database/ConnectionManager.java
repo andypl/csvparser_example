8fe47082f20cd34662d47a6a8d2d831610b6aee6
Andrzej Czerniak <andrzej.czerniak@gmail.com>
2020-04-23 23:25:26 +0200

    Added comments and move log4j basic configuration generator from another class

--- src/main/java/pl/info/czerniak/csvparser/database/ConnectionManager.java
+++ src/main/java/pl/info/czerniak/csvparser/database/ConnectionManager.java
@@ -2,19 +2,21 @@ package pl.info.czerniak.csvparser.database;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.postgresql.PGConnection;
-import org.postgresql.jdbc.PgConnection;
-
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
+/**
+ * This class provide datasource for Postgresql database. It will return connection from pool.
+ * It use HikariCP library.
+ */
 public class ConnectionManager {
 
     private static ConnectionManager connectionManager;
     private HikariDataSource dataSource;
 
     private ConnectionManager(){
+        org.apache.log4j.BasicConfigurator.configure();
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl("jdbc:postgresql://localhost:5432/csvparser_test");
         config.setDriverClassName("org.postgresql.Driver");
