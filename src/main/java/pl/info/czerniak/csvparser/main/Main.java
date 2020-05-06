e153f5a69c8d52905fdb2f477e894aed5e8e0514
Andrzej Czerniak <andrzej.czerniak@gmail.com>
2020-05-06 18:38:11 +0200

    Switch to ReaderService and PrinterService.

--- src/main/java/pl/info/czerniak/csvparser/main/Main.java
+++ src/main/java/pl/info/czerniak/csvparser/main/Main.java
@@ -1,37 +1,21 @@
 package pl.info.czerniak.csvparser.main;
 
-import pl.info.czerniak.csvparser.converter.Converter2Impl;
-import pl.info.czerniak.csvparser.converter.ConverterImpl;
-import pl.info.czerniak.csvparser.dao.DatabaseManager;
-import pl.info.czerniak.csvparser.database.ConnectionManager;
-import pl.info.czerniak.csvparser.exception.IncorrectColNumberFileException;
-import pl.info.czerniak.csvparser.printer.Printer;
-import pl.info.czerniak.csvparser.service.ParserService;
+import pl.info.czerniak.csvparser.printer.ConverterImpl;
+import pl.info.czerniak.csvparser.printer.PrinterService;
+import pl.info.czerniak.csvparser.reader.ReaderService;
 
 import javax.management.AttributeNotFoundException;
 import javax.naming.directory.NoSuchAttributeException;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
 
-    public static void main(String[] args) throws NoSuchAttributeException, IncorrectColNumberFileException, IOException, SQLException, AttributeNotFoundException {
+    public static void main(String[] args) {
 
         String inputFile = System.getProperty("user.dir")+"/doc/sample/input.csv";
-
-        List<String> headerList = Stream.of("Header1", "Header2", "Header3", "Header4", "Header5","Header6", "Header7", "Header8", "Header9", "Header10")
-                .collect(Collectors.toList());
-
-        ParserService parserService = new ParserService(inputFile, headerList);
-/*
-        DatabaseManager databaseManager = new DatabaseManager(ConnectionManager.getInstance().getConnection());
-        ConverterImpl converter = new ConverterImpl(parserService.getCSVParser(), databaseManager.getCopyIn());
-        converter.convert();
-*/
-        Printer printer = new Printer(parserService.getCSVParser(), ConnectionManager.getInstance().getConnection(), new Converter2Impl());
-        System.out.println("Printed rows to database: " + printer.print());
+        ReaderService readerService = new ReaderService();
+        PrinterService printerService = new PrinterService(readerService.getDefaultParserForFile(inputFile), new ConverterImpl());
+        System.out.println(printerService.print());
     }
 }
