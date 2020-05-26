5186b433879b845c006c195d941ee20891a81dfd
Andrzej Czerniak <andrzej.czerniak@gmail.com>
2020-05-26 23:35:27 +0200

    Removed creating connection from config. Take connection object in constructor with sql command required by Postgres CopyAPI. Replace File object in constructor with CSVParser object.

--- src/main/java/pl/info/czerniak/csvparser/parser/ParserFacade.java
+++ src/main/java/pl/info/czerniak/csvparser/parser/ParserFacade.java
@@ -2,65 +2,33 @@ package pl.info.czerniak.csvparser.parser;
 
 import org.apache.commons.csv.CSVParser;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class ParserFacade {
-    private File file;
-    private FileReader fileReader;
+    private String sql;
     private FileWriter fileWriter;
     private CSVParser csvParser;
     private Connection connection;
     private Converter converter;
-    private DatabaseManager databaseManager;
 
-    public ParserFacade(String fileName, DatabaseConfig databaseConfig, Converter converter){
-        this.file = openFile(fileName);
-        createDefaultParserForFile();
-        setConnection(databaseConfig);
+    public ParserFacade(CSVParser csvParser, Connection connection, String sql, Converter converter){
+        this.connection = connection;
+        this.csvParser = csvParser;
         this.converter = converter;
+        this.sql = sql;
     }
-
-    private File openFile(String fileName){
-        try {
-            fileReader = new FileReader(fileName);
-        } catch (FileNotFoundException e) {
-            e.getLocalizedMessage();
-        } catch (EmptyFileException e) {
-            e.getLocalizedMessage();
-        }
-        return fileReader.getFile();
-    }
-
-   private void createDefaultParserForFile(){
-       try {
-           this.csvParser = Parser.getDefaultCSVParser(this.file);
-       } catch (IOException e) {
-           e.getLocalizedMessage();
-       }
-   }
-
-   private void setConnection(DatabaseConfig databaseConfig){
-        try {
-           this.connection = ConnectionManager.getInstance(databaseConfig).getConnection();
-       } catch (SQLException e) {
-           e.getLocalizedMessage();
-       }
-   }
 
    public long write(){
        try {
-           this.fileWriter = new FileWriter(this.csvParser, this.connection, this.converter);
+           this.fileWriter = new FileWriter(this.csvParser, this.connection, this.sql, this.converter);
        } catch (SQLException e) {
-           e.printStackTrace();
+           System.err.println(e.getMessage());
        }
        try {
            return fileWriter.write();
        } catch (SQLException e) {
-           e.printStackTrace();
+           System.err.println(e.getMessage());
        }
        return 0;
    }
